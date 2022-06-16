8f421d9b88d649d164793d108eaf40d2ee2b9ba9
danieldenton <danielmdenton@gmail.com>
2022-06-16 10:33:28 -0700

    SQLite added

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class ToDoList(models.Model):
     name = models.CharField(max_length=200)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 
-def index(response):
-    return HttpResponse("<h1>working!<h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s<h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
 # Create your views here.
-
-
-def v1(response):
-    return HttpResponse("<h1>V1!<h1>")
